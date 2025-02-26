fb7e4a4229a1803f7b9e62ff8b0afbb2b1f9f45f
Henrik Holle <hholle@gmail.com>
2025-02-26 22:16:45 +0100

    dsdssdds

--- vcontrold/rootfs/etc/vcontrold/heizung.sh
+++ vcontrold/rootfs/etc/vcontrold/heizung.sh
@@ -1,8 +1,14 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bashio
 
-/usr/bin/vcontrold -s 
+MQTT_HOST=$(bashio::services mqtt "host")
+MQTT_USER=$(bashio::services mqtt "username")
+MQTT_PASSWORD=$(bashio::services mqtt "password")
 
-RESULT=`/usr/bin/vclient -h 127.0.0.1:3002 -f /etc/vcontrold/heizung.cmd -t /etc/vcontrold/heizung.tpl 2>&1`
+echo $MQTT_HOST
+
+/usr/bin/vcontrold -s
+
+RESULT=$(/usr/bin/vclient -h 127.0.0.1:3002 -f /etc/vcontrold/heizung.cmd -t /etc/vcontrold/heizung.tpl 2>&1)
 CODE=${PIPESTATUS[0]}
 
 echo $RESULT
@@ -12,20 +18,20 @@ echo $code
 
 hasError=0
 while IFS='|' read -r item value status; do
-#echo "$item" 
-#echo "$value"
-   if [ -z "$value" ] || [ -z "$status" ] || [ "$status" != "OK" ]; then
-	hasError=1
-    else
-        /usr/bin/mosquitto_pub -u $MQTT_USER -P $MQTT_PASSWORD -i vcontrol -h $MQTT_HOST -r -t "vcontrold/$item" -m "$value"
-#	echo $item
-#        echo $value
-	#curl -s -X PUT -H "Content-Type: text/plain" -d $value "http://sh:8080/rest/items/"$item"/state"
-    fi
-done <<< "$RESULT"
+  #echo "$item"
+  #echo "$value"
+  if [ -z "$value" ] || [ -z "$status" ] || [ "$status" != "OK" ]; then
+    hasError=1
+  else
+    /usr/bin/mosquitto_pub -u $MQTT_USER -P $MQTT_PASSWORD -i vcontrol -h $MQTT_HOST -r -t "vcontrold/$item" -m "$value"
+    #	echo $item
+    #        echo $value
+    #curl -s -X PUT -H "Content-Type: text/plain" -d $value "http://sh:8080/rest/items/"$item"/state"
+  fi
+done <<<"$RESULT"
 
-kill `pidof vcontrold`
+kill $(pidof vcontrold)
 
 if [ "$hasError" -eq 1 ]; then
-  printf "STATUS: %s\nRESULT: %s" "$CODE" "$RESULT" > "/var/log/Heizungsdatenfehler_$(date +"%d.%m.%Y_%H:%M:%S")"
+  printf "STATUS: %s\nRESULT: %s" "$CODE" "$RESULT" >"/var/log/Heizungsdatenfehler_$(date +"%d.%m.%Y_%H:%M:%S")"
 fi
